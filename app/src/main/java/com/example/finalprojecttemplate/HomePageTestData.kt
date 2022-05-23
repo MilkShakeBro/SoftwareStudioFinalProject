fd807306bf70f631d4cc4cd6a9e7f22cdf12c206
Steven <stevenxie42@gmail.com>
2022-05-23 11:25:40 +0800

    Finish Home page

---
+++ app/src/main/java/com/example/finalprojecttemplate/HomePageTestData.kt
@@ -0,0 +1,93 @@
+package com.example.finalprojecttemplate
+
+import android.content.res.Resources
+import androidx.core.graphics.drawable.toBitmap
+
+data class HomePageTestData(
+    val resources: Resources
+) {
+    private val imagePlaceholder = resources.getDrawable(R.drawable.ic_launcher_background).toBitmap()
+    val articlesTestCases : List<HomePageListItem> = listOf(
+        HomePageListItem(
+            id = 0,
+            displayedImage = imagePlaceholder,
+            displayedName = "Art Museum"
+        ),
+        HomePageListItem(
+            id = 1,
+            displayedImage = imagePlaceholder,
+            displayedName = "Zebrafish"
+        ),
+        HomePageListItem(
+            id = 2,
+            displayedImage = imagePlaceholder,
+            displayedName = "Teenager Problems"
+        ),
+        HomePageListItem(
+            id = 3,
+            displayedImage = imagePlaceholder,
+            displayedName = "Movies"
+        ),
+        HomePageListItem(
+            id = 4,
+            displayedImage = imagePlaceholder,
+            displayedName = "Medicines"
+        )
+    )
+
+    val vocabulariesTestCases : List<HomePageListItem> = listOf(
+        HomePageListItem(
+            id = 0,
+            displayedImage = imagePlaceholder,
+            displayedName = "Cosmetics"
+        ),
+        HomePageListItem(
+            id = 1,
+            displayedImage = imagePlaceholder,
+            displayedName = "Fruits"
+        ),
+        HomePageListItem(
+            id = 2,
+            displayedImage = imagePlaceholder,
+            displayedName = "Sports"
+        ),
+        HomePageListItem(
+            id = 3,
+            displayedImage = imagePlaceholder,
+            displayedName = "Household Appliances"
+        ),
+        HomePageListItem(
+            id = 4,
+            displayedImage = imagePlaceholder,
+            displayedName = "Animals"
+        )
+    )
+
+    val themeTestCases : List<HomePageListItem> = listOf(
+        HomePageListItem(
+            id = 0,
+            displayedImage = imagePlaceholder,
+            displayedName = "Classroom"
+        ),
+        HomePageListItem(
+            id = 1,
+            displayedImage = imagePlaceholder,
+            displayedName = "Living Room"
+        ),
+        HomePageListItem(
+            id = 2,
+            displayedImage = imagePlaceholder,
+            displayedName = "Emergency Room"
+        ),
+        HomePageListItem(
+            id = 3,
+            displayedImage = imagePlaceholder,
+            displayedName = "Police Station"
+        ),
+        HomePageListItem(
+            id = 4,
+            displayedImage = imagePlaceholder,
+            displayedName = "Shopping Mall"
+        )
+    )
+}
