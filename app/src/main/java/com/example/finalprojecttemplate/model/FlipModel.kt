26d29cd23c526c4d2af96f4ebd78d8297ad1bd96
chris <a0905907180@gmail.com>
2022-05-26 17:17:08 +0800

    flip card animation

--- app/src/main/java/com/example/finalprojecttemplate/model/FlipModel.kt
+++ app/src/main/java/com/example/finalprojecttemplate/model/FlipModel.kt
@@ -1,4 +1,10 @@
-package com.example.finalprojecttemplate.model
+package com.hamedrahimvand.flipcard.model
 
-class FlipModel {
-}
+import java.io.Serializable
+
+/**
+ *
+ *@author Hamed.Rahimvand
+ *@since 2020-02-22
+ */
+data class FlipModel(var id:Int,var name:String, var front:String, var back: String, var isFlipped: Boolean) : Serializable
