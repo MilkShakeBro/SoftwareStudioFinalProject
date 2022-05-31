f478d86eba893acf8eaacfdca217239834a19ff5
Steven <stevenxie42@gmail.com>
2022-05-31 22:11:04 +0800

    Stevenxie723 merge: Game page, home page, result page are finished roughly. #1

---
+++ app/src/main/java/com/example/finalprojecttemplate/tetris/TetrisVocabulary.kt
@@ -0,0 +1,8 @@
+package com.example.finalprojecttemplate.tetris
+
+data class TetrisVocabulary (
+    val prefixRow: String,
+    val suffixRow: String,
+    val answerRow: String,
+    val chineseMeaning: String
+)
--- app/src/main/java/com/example/finalprojecttemplate/ui/HomePageFragment.kt
+++ app/src/main/java/com/example/finalprojecttemplate/ui/HomePageFragment.kt
@@ -6,50 +6,67 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.finalprojecttemplate.HomePageListAdapter
+import com.example.finalprojecttemplate.HomePageTestData
+import com.example.finalprojecttemplate.databinding.HomePageFragmentBinding
 import com.example.finalprojecttemplate.databinding.TemplateLayoutBinding
 
 class HomePageFragment: Fragment()  {
 
-    private var binding: TemplateLayoutBinding? = null
+    private var binding: HomePageFragmentBinding? = null
+
+    private lateinit var fakeDataSource: HomePageTestData
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentBinding = TemplateLayoutBinding.inflate(inflater, container, false)
+        val fragmentBinding = HomePageFragmentBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding?.apply {
-            button1.setOnClickListener {
-                val action = HomePageFragmentDirections.actionHomePageFragmentToArticlePageFragment()
-                findNavController().navigate(action)
-            }
-            button1.text = "Article"
 
-            button2.setOnClickListener {
-                val action = HomePageFragmentDirections.actionHomePageFragmentToTimerPageFragment()
-                findNavController().navigate(action)
-            }
-            button2.text = "Timer"
+        fakeDataSource = HomePageTestData(resources)
 
-            button3.setOnClickListener {
-                val action = HomePageFragmentDirections.actionHomePageFragmentToThemePageFragment()
-                findNavController().navigate(action)
-            }
-            button3.text = "Theme"
-
-            button4.setOnClickListener {
-                val action = HomePageFragmentDirections.actionHomePageFragmentToProfilePageFragment()
-                findNavController().navigate(action)
-            }
-            button4.text = "Profile"
-
-            fragmentDescription.text = "This is HomePageFragment"
+        val articleAdapter = HomePageListAdapter { id ->
+            goToArticlePage()
         }
+
+        val vocabularyAdapter = HomePageListAdapter { id ->
+            goToVocabularyPage()
+        }
+
+        val themeAdapter = HomePageListAdapter { id ->
+            goToThemePage()
+        }
+
+        articleAdapter.submitList(fakeDataSource.articlesTestCases)
+        vocabularyAdapter.submitList(fakeDataSource.vocabulariesTestCases)
+        themeAdapter.submitList(fakeDataSource.themeTestCases)
+
+        binding?.apply {
+            readingArticleRecyclerView.adapter = articleAdapter
+            vocabulariesSetsListRecyclerView.adapter = vocabularyAdapter
+            themeListRecyclerView.adapter = themeAdapter
+        }
+    }
+
+    private fun goToArticlePage() {
+        val action = HomePageFragmentDirections.actionHomePageFragmentToArticlePageFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToVocabularyPage() {
+        val action = HomePageFragmentDirections.actionHomePageFragmentToTimerPageFragment()
+        findNavController().navigate(action)
+    }
+
+    private fun goToThemePage() {
+        val action = HomePageFragmentDirections.actionHomePageFragmentToThemePageFragment()
+        findNavController().navigate(action)
     }
 }
