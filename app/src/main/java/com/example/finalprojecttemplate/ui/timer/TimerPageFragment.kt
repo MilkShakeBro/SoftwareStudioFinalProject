63502736a3487f54251d319d85e20e912436bb48
Milkshakebro <wyes9830214@gmail.com>
2022-06-02 15:29:58 +0800

    Timer almost finished (withoud passing time argument)

--- app/src/main/java/com/example/finalprojecttemplate/ui/timer/TimerPageFragment.kt
+++ app/src/main/java/com/example/finalprojecttemplate/ui/timer/TimerPageFragment.kt
@@ -1,23 +1,31 @@
 package com.example.finalprojecttemplate.ui.timer
 
+import android.R
+import android.content.DialogInterface
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.finalprojecttemplate.databinding.TemplateLayoutBinding
+import com.example.finalprojecttemplate.databinding.TimerFragmentBinding
+
 
 class TimerPageFragment: Fragment() {
 
-    private var binding: TemplateLayoutBinding? = null
+    private var binding: TimerFragmentBinding? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentBinding = TemplateLayoutBinding.inflate(inflater, container, false)
+        val fragmentBinding = TimerFragmentBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
     }
@@ -25,32 +33,39 @@ class TimerPageFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding?.apply {
-            button1.setOnClickListener {
-                val action = TimerPageFragmentDirections.actionTimerPageFragmentToHomePageFragment()
-                findNavController().navigate(action)
-            }
-            button1.text = "Home"
+        initAllPicker()
 
-            button2.setOnClickListener {
+        binding?.apply {
+            start.setOnClickListener {
+                val hour = numPickerH.value
+                val minute = numPickerM.value
+                val second = numPickerS.value
+
+                Log.d("Hour", hour.toString())
+                Log.d("Minute", minute.toString())
+                Log.d("Second", second.toString())
+
                 val action = TimerPageFragmentDirections.actionTimerPageFragmentToFlashcardPageFragment()
                 findNavController().navigate(action)
             }
-            button2.text = "Start"
 
-            button3.setOnClickListener {
-                val action = TimerPageFragmentDirections.actionTimerPageFragmentToGameTutorialFragment()
-                findNavController().navigate(action)
-            }
-            button3.text = "Skip to game tutorial"
-
-            button4.setOnClickListener {
+            skip.setOnClickListener {
                 val action = TimerPageFragmentDirections.actionTimerPageFragmentToGamePageFragment()
                 findNavController().navigate(action)
             }
-            button4.text = "Skip to play game"
 
-            fragmentDescription.text = "This is TimerPageFragment"
         }
+    }
+
+    private fun initPicker(min: Int, max: Int, p: NumberPicker) {
+        p.minValue = min
+        p.maxValue = max
+        p.setFormatter { i -> String.format("%02d", i) }
+    }
+
+    private fun initAllPicker() {
+        binding?.let { initPicker(0, 59, it.numPickerH) }
+        binding?.let { initPicker(0, 59, it.numPickerM) }
+        binding?.let { initPicker(0, 59, it.numPickerS) }
     }
 }
