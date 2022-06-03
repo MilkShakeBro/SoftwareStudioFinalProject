835a12d123f4d9a388838befc8c3dbef946a98dc
Milkshakebro <wyes9830214@gmail.com>
2022-06-03 13:49:48 +0800

    ViewPager basic version

---
+++ app/src/main/java/com/example/finalprojecttemplate/ui/game_tutorial/GameTutorialAdapter.kt
@@ -0,0 +1,88 @@
+package com.example.finalprojecttemplate.ui.game_tutorial
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.viewpager.widget.PagerAdapter
+import com.example.finalprojecttemplate.R
+
+
+class GameTutorialAdapter(
+    private val ctx: Context?
+    ): PagerAdapter() {
+
+    override fun getCount(): Int {
+        return 5
+    }
+
+    override fun isViewFromObject(view: View, `object`: Any): Boolean {
+        return view === `object`
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+
+        val layoutInflater =
+            ctx!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+        val view: View = layoutInflater.inflate(R.layout.game_tutorial_fragment, container, false)
+        val explanation: TextView = view.findViewById(R.id.explanation)
+        val page1Button: ImageView = view.findViewById(R.id.page1)
+        val page2Button: ImageView = view.findViewById(R.id.page2)
+        val page3Button: ImageView = view.findViewById(R.id.page3)
+        val page4Button: ImageView = view.findViewById(R.id.page4)
+        val page5Button: ImageView = view.findViewById(R.id.page5)
+
+        when (position) {
+            0 -> {
+                page1Button.setImageResource(R.drawable.selected)
+                page2Button.setImageResource(R.drawable.unselected)
+                page3Button.setImageResource(R.drawable.unselected)
+                page4Button.setImageResource(R.drawable.unselected)
+                page5Button.setImageResource(R.drawable.unselected)
+                explanation.text = "Move up and down to match the word."
+            }
+            1 -> {
+                page1Button.setImageResource(R.drawable.unselected)
+                page2Button.setImageResource(R.drawable.selected)
+                page3Button.setImageResource(R.drawable.unselected)
+                page4Button.setImageResource(R.drawable.unselected)
+                page5Button.setImageResource(R.drawable.unselected)
+                explanation.text = "Press left arrow to match the word."
+            }
+            2 -> {
+                page1Button.setImageResource(R.drawable.unselected)
+                page2Button.setImageResource(R.drawable.unselected)
+                page3Button.setImageResource(R.drawable.selected)
+                page4Button.setImageResource(R.drawable.unselected)
+                page5Button.setImageResource(R.drawable.unselected)
+                explanation.text = "Press skip button to skip the game."
+            }
+            3 -> {
+                page1Button.setImageResource(R.drawable.unselected)
+                page2Button.setImageResource(R.drawable.unselected)
+                page3Button.setImageResource(R.drawable.unselected)
+                page4Button.setImageResource(R.drawable.selected)
+                page5Button.setImageResource(R.drawable.unselected)
+                explanation.text = "Chinese meaning is given on top right."
+            }
+            4 -> {
+                page1Button.setImageResource(R.drawable.unselected)
+                page2Button.setImageResource(R.drawable.unselected)
+                page3Button.setImageResource(R.drawable.unselected)
+                page4Button.setImageResource(R.drawable.unselected)
+                page5Button.setImageResource(R.drawable.selected)
+                explanation.text = "Earn the score as high as possible."
+            }
+        }
+        container.addView(view)
+        return view
+    }
+
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        container.removeView(`object` as View)
+    }
+
+}
--- app/src/main/java/com/example/finalprojecttemplate/ui/game_tutorial/GameTutorialFragment.kt
+++ app/src/main/java/com/example/finalprojecttemplate/ui/game_tutorial/GameTutorialFragment.kt
@@ -1,23 +1,26 @@
-package com.example.finalprojecttemplate.ui
+package com.example.finalprojecttemplate.ui.game_tutorial
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.example.finalprojecttemplate.databinding.TemplateLayoutBinding
+import androidx.viewpager.widget.ViewPager
+import com.example.finalprojecttemplate.databinding.GameTutorialSlideFragmentBinding
 
 class GameTutorialFragment: Fragment()  {
 
-    private var binding: TemplateLayoutBinding? = null
+    private var binding: GameTutorialSlideFragmentBinding? = null
+    private var gameTutorialAdapter: GameTutorialAdapter? = null
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val fragmentBinding = TemplateLayoutBinding.inflate(inflater, container, false)
+        val fragmentBinding = GameTutorialSlideFragmentBinding.inflate(inflater, container, false)
         binding = fragmentBinding
         return fragmentBinding.root
     }
@@ -25,13 +28,10 @@ class GameTutorialFragment: Fragment()  {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        gameTutorialAdapter = GameTutorialAdapter(this.context)
+
         binding?.apply {
-            button1.setOnClickListener {
-                val action = GameTutorialFragmentDirections.actionGameTutorialFragmentToGamePageFragment()
-                findNavController().navigate(action)
-            }
-            button1.text = "Skip"
-            fragmentDescription.text = "This is GameTutorialFragment"
+            viewpager.adapter = gameTutorialAdapter
         }
     }
 }
