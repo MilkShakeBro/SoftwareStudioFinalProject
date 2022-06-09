4ba00b170d9c17a0d26ff75732d1eba54d75ae91
chris <a0905907180@gmail.com>
2022-06-09 12:33:20 +0800

    Design man drop not done yet

--- app/src/main/java/com/example/finalprojecttemplate/ui/game_tutorial/GameTutorialAdapter.kt
+++ app/src/main/java/com/example/finalprojecttemplate/ui/game_tutorial/GameTutorialAdapter.kt
@@ -15,6 +15,7 @@ class GameTutorialAdapter(
     private val ctx: Context?,
     ): PagerAdapter() {
 
+    var index = 0
     override fun getCount(): Int {
         return 5
     }
@@ -83,7 +84,7 @@ class GameTutorialAdapter(
                 explanation.text = "Earn the score as high as possible."
                 enterButton.visibility = View.VISIBLE
                 enterButton.setOnClickListener { view ->
-                    val action = GameTutorialFragmentDirections.actionGameTutorialFragmentToGamePageFragment()
+                    val action = GameTutorialFragmentDirections.actionGameTutorialFragmentToGamePageFragment(index)
                     view.findNavController().navigate(action)
                 }
             }
