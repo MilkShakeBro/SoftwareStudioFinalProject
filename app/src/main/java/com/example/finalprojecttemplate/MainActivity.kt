4c0e0ad0b4f8f28b029d0541e2a350386e97dd4c
Angela Kuo <angelakuo0214@gmail.com>
2022-06-09 09:38:03 +0800

    font done

--- app/src/main/java/com/example/finalprojecttemplate/MainActivity.kt
+++ app/src/main/java/com/example/finalprojecttemplate/MainActivity.kt
@@ -13,5 +13,6 @@ class MainActivity : AppCompatActivity() {
 
         ActivityCompat.requestPermissions(this, arrayOf(
             android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
+
     }
 }
